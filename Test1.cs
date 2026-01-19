7014a60e06a0b73a53733770e5d5dd9de2f5bdb1
SkyCobra <zgate102@live.com>
2026-01-19 14:42:18 -0800

    Create Test1.cs

---
+++ Test1.cs
@@ -0,0 +1,15 @@
+// this is a test1.cs programm
+
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World2");
+            Console.ReadLine();
+        }//end main
+    } // end class
+    
+}// end namespace
